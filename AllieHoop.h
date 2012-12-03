0107728e8ecc3bcc97d8fa0338dfe61c663633a0
Jonah Roux <jonahroux@gmail.com>
2012-12-03 17:04:28 -0600

    Fixed cRIO slot numbers.

--- AllieHoop.h
+++ AllieHoop.h
@@ -5,9 +5,9 @@ class RobotDemo : public SimpleRobot
 {
 	//cRIO Slots
 	static const UINT8 ANALOG_SIDECAR_MODULE_1 = 1;
-	static const UINT8 DIGITAL_SIDECAR_MODULE_1 = 2;
-	static const UINT8 SOLENOID_SIDECAR_MODULE_1 = 3;
-	static const UINT8 DIGITAL_SIDECAR_MODULE_2 = 4;
+	static const UINT8 DIGITAL_SIDECAR_MODULE_1 = 1;
+	static const UINT8 SOLENOID_SIDECAR_MODULE_1 = 1;
+	static const UINT8 DIGITAL_SIDECAR_MODULE_2 = 2;
 	
 	// Sidecars
 	static const UINT8 SCOOP_MOTOR_SIDECAR = DIGITAL_SIDECAR_MODULE_1;
